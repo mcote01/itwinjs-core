5cc1405cc8e218dc836a5e662cd7d7abd5e1cd6a
Keith Bentley <33296803+kabentley@users.noreply.github.com>
2021-10-18 16:22:57 -0400

    add tests for reading json5

--- core/backend/src/test/Settings.test.ts
+++ core/backend/src/test/Settings.test.ts
@@ -5,8 +5,6 @@
 
 import { expect } from "chai";
 import { IModelTestUtils } from "./IModelTestUtils";
-import * as fs from "fs-extra";
-import { parse } from "json5";
 import { Settings, SettingsPriority } from "../settings/Settings";
 import { SettingsGroupSpec, SettingSpec, SettingsSpecRegistry } from "../settings/SettingsSpecRegistry";
 import { Mutable } from "@itwin/core-bentley";
@@ -17,7 +15,7 @@ describe.only("Settings", () => {
     groupName: "group1",
     title: "group 1 settings",
     properties: {
-      sub1: {
+      "group1.sub1": {
         type: "string",
         enum: ["va1", "alt1"],
         enumDescriptions: [
@@ -27,34 +25,33 @@ describe.only("Settings", () => {
         default: "val1",
         description: "the first value",
       },
-      sub2: {
+      "group1.sub2": {
         type: "array",
         description: "an array",
       },
-      boolVal: {
+      "group1.boolVal": {
         type: "boolean",
         default: true,
         description: "boolean defaults to true",
       },
-      strVal: {
+      "group1.strVal": {
         type: "string",
         default: "default string val",
       },
-      intVal: {
+      "group1.intVal": {
         type: "integer",
         default: 22,
       },
     },
   };
 
-  SettingsSpecRegistry.register(group1);
   const imodel1Settings = {
     "group1.sub1": "imodel1 value",
     "group1.sub2": {
       arr: ["a1", "a2"],
     },
-    "setting2": 2,
-    "setting3": "setting 3 val",
+    "group1.setting2": 2,
+    "group1.setting3": "setting 3 val",
   };
 
   const imodel2Settings = {
@@ -85,9 +82,10 @@ describe.only("Settings", () => {
 
   it("priority test", () => {
     Settings.reset();
-    Settings.add("iModel1.setting.json", SettingsPriority.iModel, imodel1Settings);
-    Settings.add("iModel2.setting.json", SettingsPriority.iModel, imodel2Settings);
-    Settings.add("iTwin.setting.json", SettingsPriority.iTwin, iTwinSettings);
+    SettingsSpecRegistry.register(group1);
+    Settings.addDictionary("iModel1.setting.json", SettingsPriority.iModel, imodel1Settings);
+    Settings.addDictionary("iModel2.setting.json", SettingsPriority.iModel, imodel2Settings);
+    Settings.addDictionary("iTwin.setting.json", SettingsPriority.iTwin, iTwinSettings);
 
     expect(Settings.getSetting<string>("group1.sub1")).equals(imodel2Settings["group1.sub1"]);
     expect(Settings.getSetting<string>("group2.setting6")).equals(iTwinSettings["group2.setting6"]);
@@ -100,29 +98,28 @@ describe.only("Settings", () => {
 
     expect(Settings.getSetting<any>("group3.obj")).deep.equals(iTwinSettings["group3.obj"]); // should be original value
     expect(Settings.getSetting<boolean>("group1.boolVal")).equals(true);
-    expect(Settings.getSetting<string>("group1.strVal")).equals(group1.properties.strVal.default);
+    expect(Settings.getSetting<string>("group1.strVal")).equals(group1.properties["group1.strVal"].default);
     expect(Settings.getSetting<number>("group1.intVal")).equals(22);
     expect(Settings.getSetting("not there")).is.undefined;
+    expect(Settings.getSetting("not there", "fallback")).equals("fallback");
 
     iTwinSettings["group2.setting6"] = "new value for 6";
-    Settings.add("iTwin.setting.json", SettingsPriority.iTwin, iTwinSettings);
+    Settings.addDictionary("iTwin.setting.json", SettingsPriority.iTwin, iTwinSettings);
     expect(Settings.getSetting<string>("group2.setting6")).equals(iTwinSettings["group2.setting6"]);
 
-    (group1.properties.strVal as Mutable<SettingSpec>).default = "new default";
+    (group1.properties["group1.strVal"] as Mutable<SettingSpec>).default = "new default";
     SettingsSpecRegistry.register(group1);
     // after re-registering, the new default should be updated
-    expect(Settings.getSetting<string>("group1.strVal")).equals(group1.properties.strVal.default);
+    expect(Settings.getSetting<string>("group1.strVal")).equals(group1.properties["group1.strVal"].default);
 
-    Settings.drop("iTwin.setting.json");
-    expect(Settings.getSetting<string>("setting6")).is.undefined;
+    Settings.dropDictionary("iTwin.setting.json");
+    expect(Settings.getSetting<string>("group2.setting6")).is.undefined;
   });
 
   it("Read Settings", () => {
     Settings.reset();
     const fileName = IModelTestUtils.resolveAssetFile("test.setting.json5");
-    const settingRaw = fs.readFileSync(fileName, "utf-8");
-    const settingFile = parse(settingRaw);
-    Settings.add(fileName, 1, settingFile);
+    Settings.addFile(fileName, SettingsPriority.application);
     expect(Settings.getSetting<string>("workbench.colorTheme")).equals("Visual Studio Light");
     const token = Settings.getSetting<any>("editor.tokenColorCustomizations");
     expect(token["[Visual Studio Light]"].textMateRules[0].settings.foreground).equals("#d16c6c");

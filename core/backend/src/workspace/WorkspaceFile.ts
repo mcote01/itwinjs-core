f61585b1cad95c5c5385db6566acdba74199fcc8
Keith Bentley <33296803+kabentley@users.noreply.github.com>
2021-10-20 08:01:02 -0400

    add WorkspaceFile

--- core/backend/src/workspace/WorkspaceFile.ts
+++ core/backend/src/workspace/WorkspaceFile.ts
@@ -6,23 +6,24 @@
  * @module Workspace
  */
 
+import { AccessToken, OpenMode } from "@itwin/core-bentley";
 import { SQLiteDb } from "../SQLiteDb";
 
 export class WorkspaceFile {
   public readonly fileName: string;
-  protected readonly lockDb = new SQLiteDb();
+  protected readonly db = new SQLiteDb();
 
   constructor(fileName: string) {
     this.fileName = fileName;
   }
 
   public create() {
-    this.lockDb.createDb(this.fileName);
-    this.lockDb.executeSQL("CREATE TABLE resources(id TEXT PRIMARY KEY NOT NULL,type TEXT,value BLOB)");
-    this.lockDb.saveChanges();
+    this.db.createDb(this.fileName);
+    this.db.executeSQL("CREATE TABLE resources(id TEXT PRIMARY KEY NOT NULL,type TEXT,value BLOB)");
+    this.db.saveChanges();
   }
 
-  public open() {
-
+  public open(args: { accessToken?: AccessToken, openMode?: OpenMode }) {
+    this.db.openDb(this.fileName, args.openMode ?? OpenMode.Readonly);
   }
 }
